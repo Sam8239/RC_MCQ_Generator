50ad1018af2ad43ec05d027857ff3ed06a2f2955
Shubham <coolshubham1999@gmail.com>
2024-04-03 12:33:06 +0530

    Added Webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './server.js', // Entry point of your application
+    output: {
+        path: path.resolve(__dirname, 'dist'), // Output directory
+        filename: 'bundle.js' // Output file name
+    },
+    resolve: {
+        fallback: {
+            "vm": false,
+            "path": false,
+            "stream": false,
+            "querystring": false,
+            "http": false,
+            "crypto": false,
+            "util": false,
+            "url": false,
+            "buffer": false,
+            "string_decoder": false,
+            "os": false,
+            "zlib": false,
+            "fs": false,
+            "async_hooks": false,
+            "net": false,
+
+        }
+    }
+};
